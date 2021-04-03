5ff03bc244cbffa8655ff7876ce2a9ce56306a25
Mykyta Batrak <fighter662@gmail.com>
2021-04-04 01:07:08 +0300

    lesson 23

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Post from './Post/Post';
+
 import styles from './MyPosts.module.css';
 
 export default function MyPosts() {
 	return (
-		<div>
-			My posts
+		<div className={styles.postsBlock}>
+			<h3>My posts</h3>
 			<div>
-				<textarea></textarea>
-				<button>Add Post</button>
+				<div>
+					<textarea></textarea>
+				</div>
+				<div>
+					<button>Add Post</button>
+				</div>
 			</div>
-			<div>
+			<div className={styles.posts}>
 				<Post message="Hi, how are you?" />
 				<Post message="It's my first post" />
 			</div>
